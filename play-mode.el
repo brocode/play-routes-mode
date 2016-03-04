0e06128ae84654dc3cf16acc53ca6e7e9083ebba
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-03-04 15:50:19 +0100

    use defvar and namespace

--- play-mode.el
+++ play-mode.el
@@ -16,20 +16,20 @@
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("/routes\\'" . play-mode))
 
-(setq routes-keywords '("GET" "POST" "DELETE" "PUT" "HEAD" "OPTIONS"))
-(setq routes-keywords-regexp (regexp-opt routes-keywords 'words))
+(defvar play-mode/routes-keywords '("GET" "POST" "DELETE" "PUT" "HEAD" "OPTIONS"))
+(defvar play-mode/routes-keywords-regexp (regexp-opt play-mode/routes-keywords 'words))
 
-(setq routes-highlights
+(defvar play-mode/routes-highlights
      `(
         ("#.*" . font-lock-comment-face)
         (":[^ /]+" . font-lock-variable-name-face)
         ("\\([a-zA-Z][a-zA-Z0-9]*\\)\\(:\\|=\\)" 1 font-lock-variable-name-face)
-        (,routes-keywords-regexp . font-lock-keyword-face)
+        (,play-mode/routes-keywords-regexp . font-lock-keyword-face)
 ))
 
 ;;;###autoload
 (define-derived-mode play-mode fundamental-mode
-  (setq font-lock-defaults '(routes-highlights))
+  (setq font-lock-defaults '(play-mode/routes-highlights))
     (setq mode-name "routes"))
 
 
