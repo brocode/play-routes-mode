940c167f5bb1e0261a67b52db7608301a62e8736
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-03-04 15:11:27 +0100

    conform to packaging conventions

--- play-mode.el
+++ play-mode.el
@@ -1,5 +1,19 @@
+;;; play-mode.el --- Play Framework Integration
+
+;; Copyright (C) 2016 Maximilien Riehl, Patrick Haun
+
+;; Author: M.Riehl <max@flatmap.ninja>, P.Haun <bomgar85@googlemail.com>
+;; Version: 0.1
+;; Package-Requires: ()
+;; Keywords: play, scala
+;; URL: https://github.com/brocode/play-mode/
+
+;;; Commentary:
+
+;; This package provides basic support for the play framework
 (defvar play-mode-hook nil)
 
+;;;###autoload
 (add-to-list 'auto-mode-alist '("/routes\\'" . play-mode))
 
 (setq routes-keywords '("GET" "POST" "DELETE" "PUT" "HEAD" "OPTIONS"))
@@ -13,9 +27,11 @@
         (,routes-keywords-regexp . font-lock-keyword-face)
 ))
 
+;;;###autoload
 (define-derived-mode play-mode fundamental-mode
   (setq font-lock-defaults '(routes-highlights))
     (setq mode-name "routes"))
 
 
 (provide 'play-mode)
+;;; play-mode.el ends here
