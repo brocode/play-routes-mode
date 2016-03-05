349f67e7ccb9939c3e11e5dc838db6258275086c
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-03-05 08:43:24 +0100

    renamed to play routes mode

--- play-mode.el
+++ play-mode.el
@@ -1,4 +1,4 @@
-;;; play-mode.el --- Play Framework Integration
+;;; play-routes-mode.el --- Play Framework Routes File Support
 
 ;; Copyright (C) 2016 Maximilien Riehl, Patrick Haun
 
@@ -6,29 +6,29 @@
 ;; Version: 0.1
 ;; Package-Requires: ()
 ;; Keywords: play, scala
-;; URL: https://github.com/brocode/play-mode/
+;; URL: https://github.com/brocode/play-routes-mode/
 
 ;;; Commentary:
 
-;; This package provides basic support for the play framework
+;; This package provides basic support for the play routes file
 
 ;;; Code:
 
 
-(defvar play-mode-routes-keywords '("GET" "POST" "DELETE" "PUT" "HEAD" "OPTIONS"))
-(defvar play-mode-routes-keywords-regexp (regexp-opt play-mode-routes-keywords 'words))
+(defvar play-routes-mode-keywords '("GET" "POST" "DELETE" "PUT" "HEAD" "OPTIONS"))
+(defvar play-routes-mode-keywords-regexp (regexp-opt play-routes-mode-keywords 'words))
 
-(defvar play-mode-routes-highlights
+(defvar play-routes-mode-highlights
   `(
     (":[^ /]+" . font-lock-variable-name-face)
     ("\\([a-zA-Z][a-zA-Z0-9]*\\)\\(:\\|=\\)" 1 font-lock-variable-name-face)
-    (,play-mode-routes-keywords-regexp . font-lock-keyword-face)
+    (,play-routes-mode-keywords-regexp . font-lock-keyword-face)
     ))
 
 ;;;###autoload
 (define-derived-mode play-routes-mode fundamental-mode " PlayRoutes"
   "Major mode for Play Framework routes files."
-  (setq font-lock-defaults '(play-mode-routes-highlights)))
+  (setq font-lock-defaults '(play-routes-mode-highlights)))
 
 (modify-syntax-entry ?# "<   " play-routes-mode-syntax-table)
 (modify-syntax-entry ?\n ">   " play-routes-mode-syntax-table)
@@ -36,5 +36,5 @@
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("/routes\\'" . play-routes-mode))
 
-(provide 'play-mode)
-;;; play-mode.el ends here
+(provide 'play-routes-mode)
+;;; play-routes-mode.el ends here
